73d1361ce66b18e1b339cab92b478278e893a86f
Kenniel-Torres <kennieltorres@gmail.com>
2024-07-04 16:42:20 -0400

    Fixed gitignore

--- csharpacademy/Project1/MathGame/Program.cs
+++ csharpacademy/Project1/MathGame/Program.cs
@@ -4,11 +4,15 @@ Random rand = new Random();
 bool canPlay;
 int a;
 int b;
+List<string> gameHistory = new List<string>();
+DateTime today = DateTime.Today;
+Console.WriteLine($"Today's date: {today}");
 
 do{
+    gameHistory.Add($"{today}");
     Console.WriteLine("Welcome to the Math Game!");
     Console.WriteLine("Pick one of the following options:");
-    Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Quit");
+    Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. View Game History\n6. Quit");
     userOption = Console.ReadLine();
     Console.WriteLine("");
 
@@ -104,6 +108,12 @@ do{
             }
             break;
         case "5":
+            Console.WriteLine("Game History:");
+            foreach(string game in gameHistory){
+                Console.WriteLine(game);
+            }
+            break;
+        case "6":
             Console.WriteLine("Goodbye!");
             break;
         default:
@@ -111,4 +121,6 @@ do{
             break;
     }
 
-}while(userOption!="5");
+}while(userOption!="6");
+
+gameHistory.Clear();
