8543e18e59e03e9367a34acc2133973c64acb780
Kenniel-Torres <kennieltorres@gmail.com>
2024-07-04 16:29:11 -0400

    Added game menu and base code for all 4 operations.

--- csharpacademy/Project1/MathGame/Program.cs
+++ csharpacademy/Project1/MathGame/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string? userOption;
+Random rand = new Random();
+bool canPlay;
+int a;
+int b;
+
+do{
+    Console.WriteLine("Welcome to the Math Game!");
+    Console.WriteLine("Pick one of the following options:");
+    Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Quit");
+    userOption = Console.ReadLine();
+    Console.WriteLine("");
+
+    switch(userOption){
+        case "1":
+            canPlay = true;
+            while(canPlay){
+                a = rand.Next(0,100);
+                b = rand.Next(0,100);
+                Console.WriteLine($"{a} + {b} = ");
+                try{
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    if(answer != a+b){
+                        canPlay = false;
+                        Console.WriteLine("Game Over! Press any key to return to the menu.");
+                        Console.ReadLine();
+                    }
+                }
+                catch{
+                    Console.WriteLine("Invalid input. Press any key to return to the menu.");
+                    Console.ReadLine(); 
+                    break;
+                }
+                Console.WriteLine("");
+            } 
+            break;
+        case "2":
+            canPlay = true;
+            while(canPlay){
+                a = rand.Next(0,100);
+                b = rand.Next(0,100);
+                Console.WriteLine($"{a} - {b} = ");
+                try{
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    if(answer != a-b){
+                        canPlay = false;
+                        Console.WriteLine("Game Over! Press any key to return to the menu.");
+                        Console.ReadLine();
+                    }
+                }
+                catch{
+                    Console.WriteLine("Invalid input. Press any key to return to the menu.");
+                    Console.ReadLine(); 
+                    break;
+                }
+                Console.WriteLine("");
+            }
+            break;
+        case "3":
+            canPlay = true;
+            while(canPlay){
+                a = rand.Next(0,100);
+                b = rand.Next(0,100);
+                Console.WriteLine($"{a} * {b} = ");
+                try{
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    if(answer != a*b){
+                        canPlay = false;
+                        Console.WriteLine("Game Over! Press any key to return to the menu.");
+                        Console.ReadLine();
+                    }
+                }
+                catch{
+                    Console.WriteLine("Invalid input. Press any key to return to the menu.");
+                    Console.ReadLine(); 
+                    break;
+                }
+                Console.WriteLine("");
+            }
+            break;
+        case "4":
+            canPlay = true;
+            while(canPlay){
+                a = rand.Next(0,100);
+                b = rand.Next(2,100);
+                while(a%b!=0){
+                    b = rand.Next(2,100);
+                }
+                Console.WriteLine($"{a} / {b} = ");
+                try{
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    if(answer != a/b){
+                        canPlay = false;
+                        Console.WriteLine("Game Over! Press any key to return to the menu.");
+                        Console.ReadLine();
+                    }
+                }catch{
+                    Console.WriteLine("Invalid input. Press any key to return to the menu.");
+                    Console.ReadLine(); 
+                    break;
+                }
+                Console.WriteLine("");
+            }
+            break;
+        case "5":
+            Console.WriteLine("Goodbye!");
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+
+}while(userOption!="5");
