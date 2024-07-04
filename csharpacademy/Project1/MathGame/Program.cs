fadc03e117a139cb094fa4c0d644234202d47b6b
Kenniel-Torres <kennieltorres@gmail.com>
2024-07-04 16:58:38 -0400

    Added option to view game history (non-persistent).

--- csharpacademy/Project1/MathGame/Program.cs
+++ csharpacademy/Project1/MathGame/Program.cs
@@ -4,12 +4,11 @@ Random rand = new Random();
 bool canPlay;
 int a;
 int b;
+int answer;
 List<string> gameHistory = new List<string>();
-DateTime today = DateTime.Today;
-Console.WriteLine($"Today's date: {today}");
+DateTime today;
 
 do{
-    gameHistory.Add($"{today}");
     Console.WriteLine("Welcome to the Math Game!");
     Console.WriteLine("Pick one of the following options:");
     Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. View Game History\n6. Quit");
@@ -18,13 +17,15 @@ do{
 
     switch(userOption){
         case "1":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             canPlay = true;
             while(canPlay){
                 a = rand.Next(0,100);
                 b = rand.Next(0,100);
                 Console.WriteLine($"{a} + {b} = ");
                 try{
-                    int answer = Convert.ToInt32(Console.ReadLine());
+                    answer = Convert.ToInt32(Console.ReadLine());
                     if(answer != a+b){
                         canPlay = false;
                         Console.WriteLine("Game Over! Press any key to return to the menu.");
@@ -40,13 +41,15 @@ do{
             } 
             break;
         case "2":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             canPlay = true;
             while(canPlay){
                 a = rand.Next(0,100);
                 b = rand.Next(0,100);
                 Console.WriteLine($"{a} - {b} = ");
                 try{
-                    int answer = Convert.ToInt32(Console.ReadLine());
+                    answer = Convert.ToInt32(Console.ReadLine());
                     if(answer != a-b){
                         canPlay = false;
                         Console.WriteLine("Game Over! Press any key to return to the menu.");
@@ -62,13 +65,15 @@ do{
             }
             break;
         case "3":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             canPlay = true;
             while(canPlay){
                 a = rand.Next(0,100);
                 b = rand.Next(0,100);
                 Console.WriteLine($"{a} * {b} = ");
                 try{
-                    int answer = Convert.ToInt32(Console.ReadLine());
+                    answer = Convert.ToInt32(Console.ReadLine());
                     if(answer != a*b){
                         canPlay = false;
                         Console.WriteLine("Game Over! Press any key to return to the menu.");
@@ -84,6 +89,8 @@ do{
             }
             break;
         case "4":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             canPlay = true;
             while(canPlay){
                 a = rand.Next(0,100);
@@ -93,7 +100,7 @@ do{
                 }
                 Console.WriteLine($"{a} / {b} = ");
                 try{
-                    int answer = Convert.ToInt32(Console.ReadLine());
+                    answer = Convert.ToInt32(Console.ReadLine());
                     if(answer != a/b){
                         canPlay = false;
                         Console.WriteLine("Game Over! Press any key to return to the menu.");
@@ -109,15 +116,21 @@ do{
             break;
         case "5":
             Console.WriteLine("Game History:");
+            if (gameHistory.Count == 0){
+                Console.WriteLine("No games played yet.");
+            }
             foreach(string game in gameHistory){
                 Console.WriteLine(game);
             }
+            Console.WriteLine("");
             break;
         case "6":
             Console.WriteLine("Goodbye!");
+            Console.WriteLine("");
             break;
         default:
             Console.WriteLine("Invalid option. Please try again.");
+            Console.WriteLine("");
             break;
     }
 
