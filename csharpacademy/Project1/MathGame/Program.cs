46f9d5516220682347c0b0f523f01353e4658753
Kenniel-Torres <kennieltorres@gmail.com>
2024-07-04 20:59:22 -0400

    Added Random Game option.

--- csharpacademy/Project1/MathGame/Program.cs
+++ csharpacademy/Project1/MathGame/Program.cs
@@ -5,6 +5,7 @@ int firstNumber;
 int secondNumber;
 int answer;
 int correctAnswer;
+int randomGame;
 List<string> gameHistory = new List<string>();
 DateTime today;
 
@@ -19,24 +20,36 @@ bool PlayGame()
 {
     Console.WriteLine("Welcome to the Math Game!");
     Console.WriteLine("Pick one of the following options:");
-    Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. View Game History\n6. Quit");
+    Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Random\n6. View Game History\n7. Quit");
     userOption = Console.ReadLine();
     Console.WriteLine("");
 
     switch (userOption)
     {
         case "1":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             return AdditionOperation();
         case "2":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             return SubtractionOperation();
         case "3":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             return MultiplicationOperation();
         case "4":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
             return DivisionOperation();
         case "5":
+            today = DateTime.Now;
+            gameHistory.Add($"Game Started: {today}");
+            return RandomOperation();
+        case "6":
             ViewGameHistory();
             return true;
-        case "6":
+        case "7":
             Console.WriteLine("Goodbye!");
             Console.WriteLine("");
             return false;
@@ -67,16 +80,32 @@ bool DivisionOperation()
     return PlayMathGame("/", (firstNumber, secondNumber) => firstNumber / secondNumber);
 }
 
+bool RandomOperation()
+{
+    randomGame = rand.Next(1,5);
+    switch(randomGame)
+    {
+        case 1:
+            return AdditionOperation();
+        case 2:
+            return SubtractionOperation();
+        case 3:
+            return MultiplicationOperation();
+        case 4:
+            return DivisionOperation();
+        default:
+            return true;
+    }
+}
+
 bool PlayMathGame(string operation, Func<int, int, int> calculateAnswer)
 {
-    today = DateTime.Now;
-    gameHistory.Add($"Game Started: {today}");
     bool canPlay = true;
 
     while (canPlay)
     {
         firstNumber = rand.Next(0, 100);
-        secondNumber = rand.Next(0, 100);
+        secondNumber = rand.Next(2, 100);
 
         if (operation == "/")
         {
@@ -98,6 +127,11 @@ bool PlayMathGame(string operation, Func<int, int, int> calculateAnswer)
                 canPlay = false;
                 Console.WriteLine("Game Over! Press any key to return to the menu.");
                 Console.ReadLine();
+                break;
+            }
+            if (userOption == "5")
+            {
+                return RandomOperation();
             }
         }
         catch
