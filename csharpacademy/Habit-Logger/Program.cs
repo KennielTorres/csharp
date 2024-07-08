375fb853f6d6a5b22a86e9d7ace1ef9784c12419
Kenniel-Torres <kennieltorres@gmail.com>
2024-07-08 14:48:15 -0400

    Habit Logger project 3 started.

---
+++ csharpacademy/Habit-Logger/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Data.Sqlite;
+
+namespace HabitLogger{
+
+    public class Program
+    {   
+        public static void Main(string[] args){
+            string connectionString = @"Data Source=habit-logger.db";
+
+            using (var connection = new SqliteConnection(connectionString)){
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS habits (
+                                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            date TEXT, 
+                                            habit TEXT, 
+                                            quantity INTEGER)";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+}
+
