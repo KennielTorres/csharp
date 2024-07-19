468a0084625c3d7df3159cf0fabadd4e5def376a
Kenniel-Torres <kennieltorres@gmail.com>
2024-07-19 14:06:18 -0400

    DrinkModel validation and formatting.

--- csharpacademy/Drinking-Logger/Models/DrinkModel.cs
+++ csharpacademy/Drinking-Logger/Models/DrinkModel.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 namespace Drinking_Logger.Models
 {
     public class DrinkModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
+        [Range(0, Double.MaxValue, ErrorMessage = "Value for {0} must be positive.")]
         public double Quantity { get; set; }
     }
 }
